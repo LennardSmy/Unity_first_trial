586ecf0bd97c5c93588546a4706c99dc888348f1
Lennard Sy <l.samuel1996@gmail.com>
2021-03-20 15:04:21 +0100

    Update Stand Video 12

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class Player : MonoBehaviour
+{   
+    
+    [SerializeField]
+    private float _speed = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = new Vector3(0, 0, 0);
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis ("Horizontal"); 
+        transform.Translate(Vector3.right * Time.deltaTime * _speed *horizontalInput);
     }
 }
